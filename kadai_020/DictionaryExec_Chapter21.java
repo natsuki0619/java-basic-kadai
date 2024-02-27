975f6ade76c0e2ce525d39bdaf28eb39a0bfc419
natsuki0619 <157789898+natsuki0619@users.noreply.github.com>
2024-02-27 16:07:32 +0900

    Add files via upload

---
+++ kadai_020/DictionaryExec_Chapter21.java
@@ -0,0 +1,12 @@
+package kadai_020;
+
+public class DictionaryExec_Chapter21 {
+
+	    public static void main(String[] args) {
+	        Dictionary_Chapter20 dictionary = new Dictionary_Chapter20();
+	        String[] words = {"apple", "banana", "grape"}; // 配列に調べたい単語をセット
+	        ((Dictionary_Chapter20) dictionary).seachArray(words);
+	    }
+	}
+
+
